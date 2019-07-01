1eb626c96d055c7e42f8819a4c5e1ab7cc8f8e8a
ombuiotimialex <ombuiotimialex@gmail.com>
2019-07-01 13:48:56 +0300

    modifying html file using javascript

--- js/index.js
+++ js/index.js
@@ -14,6 +14,12 @@ function login() {
     }
 }
 
+function pageLoaded() {
+    generateMatchData()
+    generateTransactions()
+    generateBetHistory()
+}
+
 function updateContent(selectedContainer) {
     let placeBetContainer = document.getElementById('placeBetContainer');
     let betHistoryContainer = document.getElementById('betHistoryContainer');
@@ -50,6 +56,8 @@ function updateContent(selectedContainer) {
 }
 
 function generateMatchData() {
+    let tableMatches = document.getElementById("placeBetMatches");
+
     let matches = [
         {
             teams:
@@ -185,5 +193,353 @@ function generateMatchData() {
                 }
 
         }
+    ];
+
+    for (let i = 0; i < matches.length; i++) {
+        let match = matches[i];
+
+        let matchRow = "<tr>" +
+            "<td colspan='3' align='center'>" +
+            "<div>" + match.teams[0].name + ' vs ' + match.teams[1].name + "</div>" +
+            "</td>" +
+            "</tr>" +
+            "<tr>" +
+            "<td>" +
+            "<input type='button' class='colour' value='Home " + match.teams[0].odd + "'>" +
+            "<input type='button' class='colour' value='Draw " + match.teams[2].draw + "'>" +
+            "<input type='button' class='colour' value='Away " + match.teams[1].odd + "'>" +
+            "</td>" +
+            "</tr>";
+
+        tableMatches.innerHTML += matchRow;
+    }
+}
+
+function generateTransactions() {
+    let transactions = document.getElementById("transactions");
+    let payments = [
+        {
+            transactionReference: "NSJDHDDDD",
+            amount: '28731',
+            time: '19-6-2019  09.09.00'
+        },
+        {
+            transactionReference: "TTSJIYTSY",
+            amount: '76573',
+            time: '19-8-2019'
+        },
+        {
+            transactionReference: "UYYCDASD",
+            amount: '23735',
+            time: '20-3-2019  09.01. 03'
+        },
+        {
+            transactionReference: "TTSJIYTSY",
+            amount: '4232',
+            time: '13-2-23  09. 00. 00'
+        },
+        {
+            transactionReference: "DISHDCS",
+            amount: '1333',
+            time: '21-3-2019  09.09.09'
+        },
+        {
+            transactionReference: "NSJDHDDDD",
+            amount: '28731',
+            time: '19-6-2019  09.09.00'
+        },
+        {
+            transactionReference: "TTSJIYTSY",
+            amount: '76573',
+            time: '19-8-2019'
+        },
+        {
+            transactionReference: "UYYCDASD",
+            amount: '23735',
+            time: '20-3-2019  09.01. 03'
+        },
+        {
+            transactionReference: "TTSJIYTSY",
+            amount: '4232',
+            time: '13-2-23  09. 00. 00'
+        },
+        {
+            transactionReference: "DISHDCS",
+            amount: '1333',
+            time: '21-3-2019  09.09.09'
+        },
     ]
+    for (let j = 0; j <= payments.length; j++) {
+        let payment = payments[j];
+
+        let transactionRow = "<tr>" +
+            "<td colspan='3' align='center'>" +
+            "<div>" + payment.transactionReference + payment.time + "</div>" +
+            "</td>" +
+            "<td>" +
+            "<div class='paymentsButton'>" + payment.amount +
+            "</div>" +
+            "</td>" +
+            "</tr>";
+        transactions.innerHTML += transactionRow;
+
+    }
+}
+
+function generateBetHistory() {
+    let betHistory = document.getElementById("betHistory");
+    let bets = [
+        {
+            teams:
+                [
+                    {
+                        name: "Liverpool",
+                        odd: '2.1',
+                        goals: '3'
+                    },
+                    {
+                        name: "Manchester United",
+                        odd: '3.22',
+                        goals: '1'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+
+                    {
+                        pick: '1'
+                    },
+                    {
+                        result: '1'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '2'
+                    },
+                    {
+                        name: "Liverpool",
+                        odd: '3.22',
+                        goals: '0'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '1'
+                    },
+                    {
+                        result: '1'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chile",
+                        odd: '2.1',
+                        goals: '2'
+                    },
+                    {
+                        name: "Manchester United",
+                        odd: '3.22',
+                        goals: '3'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '1'
+                    },
+                    {
+                        result: '2'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '2'
+                    },
+                    {
+                        name: "Chile",
+                        odd: '3.22',
+                        goals: "2"
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: 'X'
+                    },
+                    {
+                        result: '1'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '1'
+                    },
+                    {
+                        name: "Manchester City",
+                        odd: '3.22',
+                        goals: '3'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '1'
+                    },
+                    {
+                        result: '1'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '3'
+                    },
+                    {
+                        name: "Queen Park Rangers",
+                        odd: '3.22',
+                        goals: '4'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '2'
+                    },
+                    {
+                        result: '2'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '4'
+                    },
+                    {
+                        name: "Manchester United",
+                        odd: '3.22',
+                        goals: '4'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '2'
+                    },
+                    {
+                        result: '2'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '2'
+                    },
+                    {
+                        name: "Manchester United",
+                        odd: '3.22',
+                        goals: '2'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '1'
+                    },
+                    {
+                        result: '2'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Chelsea",
+                        odd: '2.1',
+                        goals: '3'
+                    },
+                    {
+                        name: "Postal Rangers",
+                        odd: '3.22',
+                        goals: '2'
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: '2'
+                    },
+                    {
+                        result: '1'
+                    },
+                ],
+            teams:
+                [
+                    {
+                        name: "Crystal palace",
+                        odd: '2.1',
+                        goals: '3'
+                    },
+                    {
+                        name: "Liverpool",
+                        odd: '3.22',
+                        goals: '1'
+
+                    },
+                    {
+                        draw: '2.32'
+                    },
+                    {
+                        pick: 'X'
+                    },
+                    {
+                        result: '2'
+                    },
+                ]
+        }
+    ];
+    for (let k = 0; k <= bets.length; k++) {
+        let bet = bets[k];
+
+        let betHistoryRow = "<tr>" +
+            "<td" > +
+                "<div>" + bet.teams.name + bet.teams.odd +
+            "</div>" +
+            "</td>" +
+            "<td>" +
+            "<div>" + bet.teams.draw +
+            "</div>" +
+            "</td>" +
+            "<td>" +
+            "<div>" + bet.teams.name +
+            "</div>" +
+            "</td>" +
+            "<td>" +
+            "<div>" + bet.teams.pick +
+            "</div>" +
+            "</td>" +
+            "<td>" +
+            "<div>" + bet.teams.result +
+            "</div>" +
+            "</td>" +
+            "</tr>";
+
+        betHistory.innerHTML += betHistoryRow;
+    }
 }
